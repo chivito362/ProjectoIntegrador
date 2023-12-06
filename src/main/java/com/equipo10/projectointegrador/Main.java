525d2d0af17d011cb2475c57212cf7d82692650c
chivito362 <chivito362@gmail.com>
2023-12-06 11:24:23 -0300

    cambios 2

--- src/main/java/com/equipo10/projectointegrador/Main.java
+++ src/main/java/com/equipo10/projectointegrador/Main.java
@@ -11,30 +11,33 @@ public class Main {
 
     public static void main(String[] args) {
         Controller controller = new Controller();
-        //Crear Roles
-        List<Rol> roles = List.of(new Rol(1, "RRHH"), new Rol(2, "Area Comercial"), new Rol(3, "Mesa de Ayuda"), new Rol(4, "Admin"));
-        roles.stream().forEach(c -> controller.crearRol(c));
-        //Crear Usuarios
-        List<Usuario> usuarios = List.of(
-                new Usuario("admin", "admin", roles.get(3)),
-                new Usuario("rrhh", "rrhh", roles.get(0)),
-                new Usuario("comercial", "comercial", roles.get(1)),
-                new Usuario("ayuda", "ayuda", roles.get(2)));
-        usuarios.stream().forEach(us -> controller.crearUsuario(us));
-        //Crear Especialidades
-        List<Especialidad> especialidades = List.of(
-                new Especialidad("LinuxUbuntu"),
-                new Especialidad("Windows"),
-                new Especialidad("MacOS"),
-                new Especialidad("SAP"),
-                new Especialidad("Tango"),
-                new Especialidad("Windows+Tango"),
-                new Especialidad("Linux+SAP"),
-                new Especialidad("MacOS")
-        );
-        especialidades.stream().forEach(esp -> controller.crearEspecialidad(esp));
+        
+//        //Crear Roles-----------------------
+//        List<Rol> roles = List.of(new Rol(1, "RRHH"), new Rol(2, "Area Comercial"), new Rol(3, "Mesa de Ayuda"), new Rol(4, "Admin"));
+//        roles.stream().forEach(c -> controller.crearRol(c));
 
-        //Login de acceso 
+//        //Crear Usuarios--------------------
+//        List<Usuario> usuarios = List.of(
+//                new Usuario("admin", "admin", roles.get(3)),
+//                new Usuario("rrhh", "rrhh", roles.get(0)),
+//                new Usuario("comercial", "comercial", roles.get(1)),
+//                new Usuario("ayuda", "ayuda", roles.get(2)));
+//        usuarios.stream().forEach(us -> controller.crearUsuario(us));
+
+//        //Crear Especialidades--------------
+//        List<Especialidad> especialidades = List.of(
+//                new Especialidad("LinuxUbuntu"),
+//                new Especialidad("Windows"),
+//                new Especialidad("MacOS"),
+//                new Especialidad("SAP"),
+//                new Especialidad("Tango"),
+//                new Especialidad("Windows+Tango"),
+//                new Especialidad("Linux+SAP"),
+//                new Especialidad("MacOS")
+//        );
+//        especialidades.stream().forEach(esp -> controller.crearEspecialidad(esp));
+
+        //Login de acceso--------------------
         Login login = new Login(controller);
         login.setVisible(true);
         login.setLocationRelativeTo(null);
